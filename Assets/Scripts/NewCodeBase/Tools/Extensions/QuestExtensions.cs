71f321cd67f9e191cb8dc647180231aa9b18b086
AtennopVer <anton7185569@gmail.com>
2023-02-26 21:21:08 +0300

    Move ActiveGoal getter to extensions

---
+++ Assets/Scripts/NewCodeBase/Tools/Extensions/QuestExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using Remagures.Model.QuestSystem;
+
+namespace Remagures.Tools
+{
+    public static class QuestExtensions
+    {
+        public static IQuestGoal GetActiveGoal(this IQuest quest)
+            => quest.Goals.First(goal => !goal.IsCompleted);
+    }
+}
