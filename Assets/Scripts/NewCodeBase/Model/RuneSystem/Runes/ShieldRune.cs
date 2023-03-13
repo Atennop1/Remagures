8acefbbb34eaf01d2ab9fc2683317556db7bb6f4
AtennopVer <anton7185569@gmail.com>
2023-03-13 21:57:50 +0300

    Added realization for shield rune

--- Assets/Scripts/NewCodeBase/Model/RuneSystem/Runes/ShieldRune.cs
+++ Assets/Scripts/NewCodeBase/Model/RuneSystem/Runes/ShieldRune.cs
@@ -1,17 +1,26 @@
-﻿namespace Remagures.Model.RuneSystem
+﻿using System;
+using Remagures.Model.Health;
+
+namespace Remagures.Model.RuneSystem
 {
-    public sealed class ShieldRune : IRune //TODO make after health root layer
+    public sealed class ShieldRune : IRune
     {
-        public bool IsActive { get; }
+        public bool IsActive { get; private set; }
+        private readonly ActivatableArmor _activatableArmor;
+
+        public ShieldRune(ActivatableArmor activatableArmor)
+            => _activatableArmor = activatableArmor ?? throw new ArgumentNullException(nameof(activatableArmor));
         
         public void Activate()
         {
-            
+            IsActive = true;
+            _activatableArmor.Activate();
         }
 
         public void Deactivate()
         {
-            
+            IsActive = false;
+            _activatableArmor.Deactivate();
         }
     }
 }
