4102f0a45c8da60d28c002b9488eeec2b647b8d6
AtennopVer <anton7185569@gmail.com>
2023-03-27 21:18:05 +0300

    Made shared collider for BT conditions

---
+++ Assets/Scripts/NewCodeBase/Model/AI/Enemies/Conditions/IsPlayerInBounds.cs
@@ -0,0 +1,15 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+
+namespace Remagures.Model.AI.Enemies.Conditions
+{
+    public sealed class IsPlayerInBounds : Conditional
+    {
+        public SharedEnemyWithTarget SharedEnemyWithTarget;
+        public SharedCollider2D BoundsCollider2D;
+
+        public override TaskStatus OnUpdate()
+            => BoundsCollider2D.Value.bounds.Contains(SharedEnemyWithTarget.Value.TargetData.Transform.position)
+               ? TaskStatus.Success
+               : TaskStatus.Failure;
+    }
+}
---
+++ Assets/Scripts/NewCodeBase/Model/AI/Enemies/SharedCollider2D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+namespace Remagures.Model.AI.Enemies
+{
+    [Serializable]
+    public sealed class SharedCollider2D : SharedVariable<Collider2D>
+    {
+        public static implicit operator SharedCollider2D(Collider2D value) 
+            => new() { Value = value };
+    }
+}
