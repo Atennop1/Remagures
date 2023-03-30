d8eeb1f6a1fcf81f1706786e0c4ce38393911f4c
AtennopVer <anton7185569@gmail.com>
2023-03-30 23:24:18 +0300

    Made saving for MaxHealth

---
+++ Assets/Scripts/NewCodeBase/Model/Health/Decorators/MaxHealthWithSaving.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SaveSystem;
+
+namespace Remagures.Model.Health
+{
+    public sealed class MaxHealthWithSaving : IMaxHealth
+    {
+        public int Value => _maxHealth.Value;
+        
+        private readonly IMaxHealth _maxHealth;
+        private readonly ISaveStorage<int> _storage;
+
+        public MaxHealthWithSaving(IMaxHealth maxHealth, ISaveStorage<int> storage)
+        {
+            _maxHealth = maxHealth ?? throw new ArgumentNullException(nameof(maxHealth));
+            _storage = storage ?? throw new ArgumentNullException(nameof(storage));
+        }
+
+        public bool CanIncrease(int amount) 
+            => _maxHealth.CanIncrease(amount);
+
+        public bool CanDecrease(int amount) 
+            => _maxHealth.CanDecrease(amount);
+
+        public void Increase(int amount)
+        {
+            _maxHealth.Increase(amount);
+            _storage.Save(Value);
+        }
+
+        public void Decrease(int amount)
+        {
+            _maxHealth.Decrease(amount);
+            _storage.Save(Value);
+        }
+    }
+}
---
+++ Assets/Scripts/NewCodeBase/Root/HealthSystem/MaxHealth/MaxHealthWithSavingFactory.cs
@@ -0,0 +1,23 @@
+﻿using Remagures.Model.Health;
+using SaveSystem;
+using SaveSystem.Paths;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Remagures.Root
+{
+    public sealed class MaxHealthWithSavingFactory : SerializedMonoBehaviour, IMaxHealthFactory
+    {
+        [SerializeField] private IMaxHealthFactory _maxHealthFactory;
+        private IMaxHealth _builtHealth;
+
+        public IMaxHealth Create()
+        {
+            if (_builtHealth != null)
+                return _builtHealth;
+
+            _builtHealth = new MaxHealthWithSaving(_maxHealthFactory.Create(), new BinaryStorage<int>(new Path("MaxPlayerHealth")));
+            return _builtHealth;
+        }
+    }
+}
