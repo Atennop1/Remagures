d8d631e0aaa4ce19f35e2116ecf1c458e481e408
AtennopVer <anton7185569@gmail.com>
2023-05-02 21:13:19 +0300

    Added buttons for meat system

---
+++ Assets/Source/Model/UI/Buttons/MeatSystem/AddMeatButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Remagures.Model.MeatSystem;
+
+namespace Remagures.Model.UI
+{
+    public sealed class AddMeatButton : IButton
+    {
+        private readonly IMeatCooker _meatCooker;
+
+        public AddMeatButton(IMeatCooker meatCooker) 
+            => _meatCooker = meatCooker ?? throw new ArgumentNullException(nameof(meatCooker));
+
+        public void Press() 
+            => _meatCooker.AddRawMeat();
+    }
+}
---
+++ Assets/Source/Model/UI/Buttons/MeatSystem/TakeMeatButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Remagures.Model.MeatSystem;
+
+namespace Remagures.Model.UI
+{
+    public sealed class TakeMeatButton : IButton
+    {
+        private readonly ICookedMeatHeap _cookedMeatHeap;
+
+        public TakeMeatButton(ICookedMeatHeap cookedMeatHeap) 
+            => _cookedMeatHeap = cookedMeatHeap ?? throw new ArgumentNullException(nameof(cookedMeatHeap));
+
+        public void Press() 
+            => _cookedMeatHeap.Take(_cookedMeatHeap.Count);
+    }
+}
