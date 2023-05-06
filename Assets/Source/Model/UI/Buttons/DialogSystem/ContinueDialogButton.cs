7ed2bd2a1430f1e9a26854842268925ff32fe65e
AtennopVer <anton7185569@gmail.com>
2023-05-06 18:25:48 +0300

    Added buttons for dialog system

---
+++ Assets/Source/Model/UI/Buttons/DialogSystem/ContinueDialogButton.cs
@@ -0,0 +1,12 @@
+﻿using Remagures.Model.DialogSystem;
+
+namespace Remagures.Model.UI
+{
+    public sealed class ContinueDialogButton : IButton
+    {
+        private readonly IDialogPlayer _dialogPlayer;
+        
+        public void Press() 
+            => _dialogPlayer.ContinueDialog();
+    }
+}
