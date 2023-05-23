0eebbc85fb336f60e26fabf7151a4e4d84f41c07
AtennopVer <anton7185569@gmail.com>
2023-05-23 12:42:30 +0300

    Added SharedMagicApplier

---
+++ Assets/Source/Model/Character/BehaviourTree/SharedVariables/SharedMagicApplier.cs
@@ -0,0 +1,10 @@
+﻿using BehaviorDesigner.Runtime;
+
+namespace Remagures.Model.Character.BehaviourTree
+{
+    public sealed class SharedMagicApplier : SharedVariable<CharacterMagicApplier>
+    {
+        public static SharedMagicApplier FromCharacterMagicApplier(CharacterMagicApplier value) 
+            => new() { Value = value };
+    }
+}
